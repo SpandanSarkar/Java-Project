39fec635767ccf9da4f6f57872f4d7b1507b7399
SpandanSarkar <51871877+SpandanSarkar@users.noreply.github.com>
2019-08-06 16:09:08 +0600

    JAVA code

---
+++ MyConnection.java
@@ -0,0 +1,27 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sunanda
+ */
+public class MyConnection {
+    public static Connection getConnection(){
+    Connection con = null;
+    try{
+    Class.forName("com.mysql.cj.jdbc.Driver");
+    con = DriverManager.getConnection("jdbc:mysql://localhost:8889/project","root","root");
+    } catch(ClassNotFoundException | SQLException ex){
+    System.out.println(ex.getMessage());
+    }
+    return con;
+    }
+}
